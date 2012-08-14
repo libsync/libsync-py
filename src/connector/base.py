23df6808542d398e96f2f0043fa87a795bf33103
Marcell Vazquez-Chanlatte <mvcisback@gmail.com>
2012-08-13 23:53:55 -0700

    Proto api for connector
    Reason: https://github.com/libsync/libsync/issues/3

---
+++ src/connector/base.py
@@ -0,0 +1,37 @@
+"""
+"""
+
+class connector(object):
+    """
+    """
+
+    def __init__(self, url):
+        """
+
+        :param url:
+        """
+        self._url = url
+
+
+    def push(self, ):
+        """
+        """
+        pass
+
+
+    def pull(self, ):
+        """
+        """
+        pass
+
+
+    def get(self, ):
+        """
+        """
+        pass
+
+
+    def put(self, ):
+        """
+        """
+        pass
